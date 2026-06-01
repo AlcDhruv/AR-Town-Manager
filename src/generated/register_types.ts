55d9a55c77765399306b4d696dd51dbe865810d7
AlcDhruv <dhruvhadap14@gmail.com>
2026-06-01 17:15:50 +0530

    More changes

---
+++ src/generated/register_types.ts
@@ -0,0 +1,19 @@
+﻿/* eslint-disable */
+import { TypeStore } from "@needle-tools/engine"
+
+// Import types
+import { OrbitBlocker } from "../scripts/OrbitBlocker.js";
+import { PrefabRotator } from "../scripts/PrefabRotator.js";
+import { ShowcaseOverlay } from "../scripts/ShowcaseOverlay.js";
+import { ShowcaseToggler } from "../scripts/ShowcaseToggler.js";
+import { TownElementSelector } from "../scripts/TownElementSelector.js";
+
+// Register types
+export function registerTypes() {
+	TypeStore.add("OrbitBlocker", OrbitBlocker);
+	TypeStore.add("PrefabRotator", PrefabRotator);
+	TypeStore.add("ShowcaseOverlay", ShowcaseOverlay);
+	TypeStore.add("ShowcaseToggler", ShowcaseToggler);
+	TypeStore.add("TownElementSelector", TownElementSelector);
+}
+registerTypes();
